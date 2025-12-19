51698349000b21e8ac3b8a80751ab92c5ee2f28b
fannnzhang <fannnzhang@gmail.com>
2025-12-19 16:17:26 +0800

    Squashed 'third_party/mars/' content from commit 6aa5b56
    git-subtree-dir: third_party/mars
    git-subtree-split: 6aa5b567afada93936252bac2f271c4b43eccd1f

---
+++ mars/comm/verinfo.h
@@ -0,0 +1,12 @@
+
+#ifndef Mars_verinfo_h
+#define Mars_verinfo_h
+
+#define MARS_REVISION "38eea3ee"
+#define MARS_PATH "release/2024-t2.1"
+#define MARS_URL ""
+#define MARS_BUILD_TIME "2024-07-30 17:02:21"
+#define MARS_TAG ""
+#define MARS_BUILD_TIMESTAMP 1722330141
+
+#endif
